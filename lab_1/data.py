1287d8c6ae813b11b7f5a6052b2b200e149c0eba
Domagoj Krivosic <domagoj@Domagojs-MacBook-Pro.local>
2016-10-22 17:04:48 +0200

    First assignment

---
+++ lab_1/data.py
@@ -0,0 +1,11 @@
+def sample_gmm_2d(K, C, N):
+    X = np.zeros((K*N, 2), dtype=float)
+    Y = np.zeros((K*N), dtype=float)
+    for i in range(K):
+        mean = np.random.uniform(-10, 10)
+        sigma = np.random.uniform(0, 5)
+        c_i = random.sample(range(C), 1)
+        for j in range(N):
+            X[i*N+j] = np.random.uniform(mean, sigma, 2)
+            Y[i*N+j] = c_i[0]
+    return (X, Y)
