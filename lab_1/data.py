21837dc5688f7d63d1982c2f0ffc2df49a2a037a
Domagoj Krivosic <domagoj@Domagojs-MacBook-Pro.local>
2016-10-26 20:29:15 +0200

    Move plotting functions and model evaluating function to data module

--- lab_1/data.py
+++ lab_1/data.py
@@ -1,5 +1,7 @@
 import numpy as np
 import random
+import matplotlib
+import matplotlib.pyplot as plt
 
 def sample_gmm_2d(K, C, N):
     X = np.zeros((K*N, 2), dtype=float)
@@ -12,3 +14,41 @@ def sample_gmm_2d(K, C, N):
             X[i*N+j] = np.random.uniform(mean, sigma, 2)
             Y[i*N+j] = c_i[0]
     return (X, Y)
+
+def eval_perf_binary(Y, Y_):
+    accuracy = float(sum(Y == Y_)) / len(Y)
+    precision = float(sum((Y == Y_) == (Y == 1))) / sum(Y)
+    recall = float(sum((Y == Y_) == (Y == 1))) / sum(Y_)
+    precisions = []
+    yy = Y == Y_
+    correct = 0
+    for i in range(len(yy)):
+        if yy[i]:
+            correct += 1
+            precisions.append(correct / (i + 1))
+    avg_precision = np.mean(precisions)
+    return accuracy, precision, recall, avg_precision
+
+def graph_data(X, Y_, Y, special=[]):
+    norm = matplotlib.colors.Normalize(vmin=0, vmax=np.max(Y_))
+    for i in range(X.shape[0]):
+        if Y_[i] == Y[i]:
+            marker = 'o'
+        else:
+            marker = 's'
+        if i in special:
+            size = 40
+        else:
+            size = 20
+        plt.scatter(X[i, 0], X[i, 1], marker=marker, c=Y_[i], cmap='Greys', norm=norm, s=size)
+    plt.show()
+
+def graph_surface(fun, rect, offset=0.0, width=1000, height=1000):
+    x1 = np.linspace(rect[0][0], rect[1][0], height)
+    x2 = np.linspace(rect[0][1], rect[1][1], width)
+    xx, yy = np.meshgrid(x1, x2)
+    h = fun(np.stack([xx.flatten(), yy.flatten()], axis=1))
+    h = h.reshape(xx.shape)
+    plt.contour(xx, yy, h, colors='black', levels=[offset])
+    norm = matplotlib.colors.Normalize(vmin=offset+min(rect[0][0], rect[0][1]),vmax=offset+max(rect[1][0], rect[1][1]))
+    plt.pcolormesh(xx, yy, h, norm=norm)
