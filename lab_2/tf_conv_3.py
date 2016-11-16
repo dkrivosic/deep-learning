c34cb30ceee57faf07366c2838b66998971b6fc0
Domagoj Krivosic <domagoj@Domagojs-MacBook-Pro.local>
2016-11-16 13:26:18 +0100

    Add drawing filters

--- lab_2/tf_conv_3.py
+++ lab_2/tf_conv_3.py
@@ -1,5 +1,13 @@
 import tensorflow as tf
+import numpy as np
+import math
+import skimage as ski
+import skimage.io
+import os
 from tensorflow.examples.tutorials.mnist import input_data
+
+DATA_DIR = '/Users/domagoj/fer/deep-learning/lab_2/data'
+SAVE_DIR = '/Users/domagoj/fer/deep-learning/lab_2/save/zad_3'
 
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
@@ -15,12 +23,41 @@ def conv2d(x, W):
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-DATA_DIR = '/Users/domagoj/fer/deep-learning/lab_2/data'
+def draw_conv_filters(epoch, step, W, save_dir):
+  C = W.shape[2]
+  w = W.copy()
+  num_filters = w.shape[3]
+  k = w.shape[1]
+  w = w.reshape(num_filters, C, k, k)
+  w -= w.min()
+  w /= w.max()
+  border = 1
+  cols = 8
+  rows = math.ceil(num_filters / cols)
+  width = cols * k + (cols-1) * border
+  height = rows * k + (rows-1) * border
+  #for i in range(C):
+  for i in range(1):
+    img = np.zeros([height, width])
+    for j in range(num_filters):
+      r = int(j / cols) * (k + border)
+      c = int(j % cols) * (k + border)
+      img[r:r+k,c:c+k] = w[j,i]
+    filename = '%s_epoch_%02d_step_%06d_input_%03d.png' % ('conv1', epoch, step, i)
+    ski.io.imsave(os.path.join(save_dir, filename), img)
+
+
 mnist = input_data.read_data_sets(DATA_DIR, one_hot=True)
 num_classes = 10
-weight_decay = 0.1
 batch_size = 50
-epochs = 100
+num_batches = mnist.train.num_examples // batch_size
+epochs = 8
+weight_decay = 1e-3
+
+
+global_step = tf.Variable(1e-1, trainable=False)
+learning_rate = learning_rate = tf.train.exponential_decay(1e-1, global_step,
+                                           epochs * num_batches, 0.96, staircase=True)
 
 y_ = tf.placeholder(tf.float32, [None, num_classes])
 x = tf.placeholder(tf.float32, [None, 784])
@@ -52,20 +89,18 @@ with tf.Session() as sess:
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_) +
         weight_decay * tf.nn.l2_loss(W_conv1) + weight_decay * tf.nn.l2_loss(W_conv2) +
         weight_decay * tf.nn.l2_loss(W_fc1) + weight_decay * tf.nn.l2_loss(W_fc2))
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy, global_step=global_step)
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     sess.run(tf.initialize_all_variables())
 
     for epoch in range(epochs):
-        print("Epoch %d"%(epoch))
-        num_batches = mnist.train.num_examples // batch_size
         for i in range(num_batches):
             batch = mnist.train.next_batch(batch_size)
-            if (i+1)%100 == 0:
+            train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+            if i%100 == 0:
                 train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1]})
-                print("step %d/%d, training accuracy %g"%(i+1, mnist.train.num_examples//batch_size, train_accuracy))
-                train_step.run(feed_dict={x: batch[0], y_: batch[1]})
-
+                print("epoch %d, step %d/%d, training accuracy %g"%(epoch, i, mnist.train.num_examples//batch_size, train_accuracy))
+                draw_conv_filters(epoch, i, W_conv1.eval(), SAVE_DIR)
     print("test accuracy %g"%accuracy.eval(feed_dict={
     x: mnist.test.images, y_: mnist.test.labels}))
